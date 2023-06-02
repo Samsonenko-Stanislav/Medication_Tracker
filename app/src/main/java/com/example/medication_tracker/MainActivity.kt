acd6c859209d40e9f03809142277dd796ff7c827
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2023-06-02 22:40:53 +0300

    Added MainActivity prototype

--- app/src/main/java/com/example/medication_tracker/MainActivity.kt
+++ app/src/main/java/com/example/medication_tracker/MainActivity.kt
@@ -1,43 +1,82 @@
 package com.example.medication_tracker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.medication_tracker.ui.theme.Medication_TrackerTheme
+import androidx.compose.ui.unit.dp
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private val medicines = mutableStateListOf(
+        Medicine("Лекарство 1"),
+        Medicine("Лекарство 2"),
+        Medicine("Лекарство 3")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Medication_TrackerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            MedicineListScreen(medicines = medicines) { medicine ->
+                MedicineDetailScreen(medicine = medicine)
+            }
+        }
+    }
+}
+
+data class Medicine(val name: String)
+@Composable
+fun MainContent(medicines: List<Medicine>, onItemClick: (Medicine) -> Unit) {
+    MedicineListScreen(medicines = medicines, onItemClick = onItemClick)
+}
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MedicineListScreen(medicines: List<Medicine>, onItemClick: (Medicine) -> Unit) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Список лекарств") })
+        }
+    ) {
+        Column {
+            for (medicine in medicines) {
+                MedicineListItem(medicine = medicine, onItemClick = onItemClick)
+                Divider()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MedicineListItem(medicine: Medicine, onItemClick: (Medicine) -> Unit) {
+    Text(
+        text = medicine.name,
+        modifier = Modifier
+            .padding(16.dp)
+            .clickable { onItemClick(medicine) }
+    )
 }
 
-@Preview(showBackground = true)
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun DefaultPreview() {
-    Medication_TrackerTheme {
-        Greeting("Android")
+fun MedicineDetailScreen(medicine: Medicine) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Детали лекарства") })
+        }
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = "Название: ${medicine.name}")
+            Spacer(modifier = Modifier.padding(8.dp))
+            Button(onClick = { }) {
+                Text(text = "Принять")
+            }
+        }
     }
 }
